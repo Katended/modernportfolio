47523fdf08fef6aa9c34d5d944c81611e0af9b9f
David Katende <katendedavidjoga@gmail.com>
2023-11-21 17:22:31 +0300

    Add transparency on menu

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+$(window).load(function () {
+	$('#preloader').fadeOut('slow', function () { $(this).remove(); });
 });
 
 
 /******************************************************************************************************************************
 Learn More Page Scroll
 *******************************************************************************************************************************/
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function () {
+	$('a.page-scroll').bind('click', function (event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
 });
 
 /******************************************************************************************************************************
 Menu
-*******************************************************************************************************************************/ 
-(function() {
+*******************************************************************************************************************************/
+(function () {
 
 	var bodyEl = document.body,
-		//content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
+		content = document.querySelector('.content-wrap'),
+		openbtn = document.getElementById('open-button'),
+		closebtn = document.getElementById('close-button'),
 		isOpen = false;
 
 	function init() {
@@ -32,27 +32,27 @@ Menu
 	}
 
 	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
+		openbtn.addEventListener('click', toggleMenu);
+		if (closebtn) {
+			closebtn.addEventListener('click', toggleMenu);
 		}
 
-		/* close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener('click', function (ev) {
 			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
+			if (isOpen && target !== openbtn) {
 				toggleMenu();
 			}
-		} );
-		*/
+		});
+
 	}
 
 	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
+		if (isOpen) {
+			classie.remove(bodyEl, 'show-menu');
 		}
 		else {
-			classie.add( bodyEl, 'show-menu' );
+			classie.add(bodyEl, 'show-menu');
 		}
 		isOpen = !isOpen;
 	}
